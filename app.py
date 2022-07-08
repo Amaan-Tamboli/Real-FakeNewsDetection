49bcf8af6dbed1278df96d3a56409795f50325fb
Amaan-Tamboli <89582825+Amaan-Tamboli@users.noreply.github.com>
2022-07-08 16:53:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import joblib
+
+# Load the model
 model = joblib.load('Real-Fake')
 st.title('Real-Fake News Detection')
+
+# user input
 ip = st.text_input('Enter the message : ')
 st.subheader('Note:- ')
 st.caption('The input format should be : title - text')
+
+# output = prediction if the user input is real or fake news
 op = model.predict([ip])
 if st.button('Predict'):
   st.title(op[0])
